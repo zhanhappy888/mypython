0a9b41c7b02ea4edc12f7f1a70d111f5715d00a6
zhanhappy888 <zhanjiang@ruc.edu.cn>
2017-07-24 15:24:56 +0800

    2017/7/24

---
+++ helloowrld.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+if (condition) {
+    
+} else {
+    
+}
+array.forEach(function(element) {
+    
+}, this);
+
+for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+    
+}
