8856ff84ad0e7ed58bd488463201b2b4a2a8de34
zhanhappy888 <zhanjiang@ruc.edu.cn>
2017-07-24 20:19:37 +0800

    git push origin master

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print("hello World")
+x = int(input('Enter an integer:'))
+ans = 0
+while ans**3 < abs(x):
+    ans = ans +1
+if ans**3 != abs(x):
+    print (x, 'is not a perfect cube')
+else:
+    if x< 0:
+        ans = -ans
+    print ("cube root of",x,' is ',ans)
